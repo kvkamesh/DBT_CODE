24d2978ee0f2640cd2752c55a9dbeb2ff991a48a
KAMESH KV <kameshkv1994@gmail.com>
2025-07-05 17:38:15 +0000

    commit

--- models/stage/TGT_ORDER_DATA_MERGE.sql
+++ models/stage/TGT_ORDER_DATA_MERGE.sql
@@ -1,5 +1,7 @@
 {{config(
 materialized = 'incremental',
-incremental_strategy = 'merge'
+incremental_strategy = 'delete+insert',
+unique_key = 'id'
 )}}
 select * from {{source("stage_to_target_load",'STAGE_ORDER_DATA')}}
+where id in (2)
